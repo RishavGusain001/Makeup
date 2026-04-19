aa2160277906bf234c8ed05fbba808a90526c134
RishavGusain001 <rishavgusain001@gmail.com>
2026-04-19 14:46:36 +0530

    fix: add vercel.json and correct root directory + fix: remove unused Pillow and Flask-MySQLdb

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://makeup-7aip.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
